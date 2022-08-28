55b0a80c4eedc04105c289096d3d262f583fc937
CTV Nguyen Van Hoang <hoangnvpt02@gmail.com>
2022-08-28 17:06:25 +0700

    done

---
+++ Assignment2/Assignment2/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            TempData["Name"] = "Nguyễn Văn Hoàng";
+            TempData["Age"] = "20";
+            TempData["Address"] = "Cầu Giấy, Hà Nội";
+            TempData.Keep();
+            return View("ShowData");
+        }
+
+        public ActionResult ShowData()
+        {
+            TempData.Keep();
+            return View();
+        }
+    }
+}
