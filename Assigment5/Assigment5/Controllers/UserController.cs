749dd4f97f25fd9d2399cb893d686e021828afbc
CTV Nguyen Van Hoang <hoangnvpt02@gmail.com>
2022-09-07 20:21:04 +0700

    updated

---
+++ Assigment5/Assigment5/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Assigment5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assigment5.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private static List<UserModel> _users = new List<UserModel>();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(_users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            UserModel user = _users.Find(emp => emp.Idd == id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                UserModel user = new UserModel();
+                user.Idd = Int32.Parse(collection["Idd"]);
+                user.ConfirmPassword = collection["ConfirmPassword"];
+                user.Email = collection["email"];
+                user.Password = collection["password"];
+                user.Name = collection["name"];
+                _users.Add(user);
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Assigment5/Assigment5/Models/UserModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assigment5.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public int Idd { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Nhập tối thiểu ít nhất 6 kí tự")]
+        [DisplayName("Tên")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Nhập tối thiểu 5 kí tự")]
+        [DisplayName("Mật khẩu")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Nhập tối thiểu 5 kí tự")]
+        [DisplayName("Mật khẩu")]
+        [Compare("Password", ErrorMessage = "Mật khẩu không trùng khớp")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email cá nhân")]
+        public string Email { get; set; }
+
+    }
+}
